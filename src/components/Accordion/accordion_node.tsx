a04bc18a382ecf3675d8ab77d69b87345054819f
Usov <usov.lesha2017@gmail.com>
2024-04-28 22:43:49 +0300

    fix accardeon photos

--- src/components/Accordion/accordion_node.tsx
+++ src/components/Accordion/accordion_node.tsx
@@ -64,8 +64,11 @@ export const Accordion_node = (
                     ref={itemRef}
                 >
                     <div className="acc_i_photo"
+                         style={{
+                                backgroundImage:`url(${url})`,
+
+                         }}
                     >
-                        <img src={url} alt=""/>
                     </div>
                     <div className="acc_i_info">
                         <h2 className="acc_top_l">0{id}</h2>
